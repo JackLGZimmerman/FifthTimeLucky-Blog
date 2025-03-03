3993320bdfd08afe955c99fd0d3a1d9a327c7869
JackLGZimmerman <51673752+JackLGZimmerman@users.noreply.github.com>
2025-03-03 18:27:28 +0000

    Delete myapp/.svelte-kit directory

--- myapp/.svelte-kit/generated/client/nodes/3.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/articles/theory/[slug]/+page.svelte";
--- myapp/.svelte-kit/types/src/routes/proxy+layout.server.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-import type { LayoutServerLoad } from './$types.js'
-
-export const load = async ({ locals }: Parameters<LayoutServerLoad>[0]) => {
-	return {
-		session: locals.session,
-		user: locals.user,
-	};
-};
